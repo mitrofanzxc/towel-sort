3be4f095ea45fd76f5c0ebb406a6a01673e05def
mitrofanzxc <mitrofanzxc@gmail.com>
2022-01-24 17:43:22 +0300

    feat: trying to make this shaitan machine work

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    let arr = [];
+    for (var i = 0; i < matrix.length; i++) {
+      if (Array.isArray(matrix[i])) {
+        arr = arr.concat(towelSort(matrix[i]));
+      } else {
+        arr.push(matrix[i]);
+      }
+    }
+    return arr.sort((a, b) => a - b);
+  }
